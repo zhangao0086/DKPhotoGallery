b8ad33cfbb4429d3c6be9fcefad2d6b09c806fe9
Bannings <zhangao0086@gmail.com>
2017-09-15 16:22:11 +0800

    Adding DKPhotoPlayerPreviewVC.

---
+++ DKPhotoGallery/Preview/PlayerPreview/DKPhotoPlayerPreviewVC.swift
@@ -0,0 +1,25 @@
+//
+//  DKPhotoPlayerPreviewVC.swift
+//  DKPhotoGalleryDemo
+//
+//  Created by ZhangAo on 15/09/2017.
+//  Copyright © 2017 ZhangAo. All rights reserved.
+//
+
+import UIKit
+
+class DKPhotoPlayerPreviewVC: DKPhotoBasePreviewVC {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+}
