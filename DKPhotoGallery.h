36b2e684bdacb65f24ad5b7b51bf7b6e4944257a
Bannings <zhangao0086@gmail.com>
2018-09-28 17:52:19 +0800

    carthage

--- DKPhotoGallery.h
+++ DKPhotoGallery.h
@@ -16,38 +16,6 @@ FOUNDATION_EXPORT const unsigned char DKPhotoGalleryVersionString[];
 
 // In this header, you should import all the public headers of your framework using statements like #import <DKPhotoGallery/PublicHeader.h>
 
-//// FLAnimatedImageView
-//#import "DKPhotoGallery/FLAnimatedImageView.h"
-//#import "DKPhotoGallery/FLAnimatedImage.h"
-//
-//// SDWebImage
-//#import "DKPhotoGallery/SDImageCache.h"
-//#import "DKPhotoGallery/SDWebImageDownloader.h"
-//#import "DKPhotoGallery/SDWebImageCoderHelper.h"
-//#import "DKPhotoGallery/SDAnimatedImageRep.h"
-//#import "DKPhotoGallery/SDWebImageCoder.h"
-//#import "DKPhotoGallery/SDWebImageCodersManager.h"
-//#import "DKPhotoGallery/SDWebImageDownloaderOperation.h"
-//#import "DKPhotoGallery/SDWebImageGIFCoder.h"
-//#import "DKPhotoGallery/SDWebImageImageIOCoder.h"
-//#import "DKPhotoGallery/SDWebImageManager.h"
-//#import "DKPhotoGallery/SDWebImagePrefetcher.h"
-//#import "DKPhotoGallery/SDWebImageTransition.h"
-//#import "DKPhotoGallery/SDWebImageWebPCoder.h"
-//#import "DKPhotoGallery/UIImage+MultiFormat.h"
-//#import "DKPhotoGallery/FLAnimatedImageView+WebCache.h"
-//#import "DKPhotoGallery/MKAnnotationView+WebCache.h"
-//#import "DKPhotoGallery/NSButton+WebCache.h"
-//#import "DKPhotoGallery/NSImage+WebCache.h"
-//#import "DKPhotoGallery/UIButton+WebCache.h"
-//#import "DKPhotoGallery/UIImage+ForceDecode.h"
-//#import "DKPhotoGallery/UIImage+GIF.h"
-//#import "DKPhotoGallery/UIImage+WebP.h"
-//#import "DKPhotoGallery/UIImageView+HighlightedWebCache.h"
-//#import "DKPhotoGallery/UIImageView+WebCache.h"
-//#import "DKPhotoGallery/UIView+WebCache.h"
-//#import "DKPhotoGallery/UIView+WebCacheOperation.h"
-
 // FLAnimatedImageView
 #import "FLAnimatedImageView.h"
 #import "FLAnimatedImage.h"
