c6fdc78cfc7c16d56ea91a9e02f391f64f046ef8
Yuki Nagao <5319256+zrn-ns@users.noreply.github.com>
2021-01-17 18:16:05 +0900

    Support SwiftPM

---
+++ Package.swift
@@ -0,0 +1,22 @@
+// swift-tools-version:5.3
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "DKPhotoGallery",
+    products: [
+        .library(
+            name: "DKPhotoGallery",
+            targets: ["DKPhotoGallery"]),
+    ],
+    dependencies: [
+        .package(url: "https://github.com/kirualex/SwiftyGif.git", from: "5.3.0"),
+    ],
+    targets: [
+        .target(
+            name: "DKPhotoGallery",
+            dependencies: [],
+            path: "DKPhotoGallery"),
+    ]
+)
